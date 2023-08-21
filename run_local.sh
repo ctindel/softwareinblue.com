c73c7ef5283bc0d87bfd3d15792613e0b68d04d9
ctindel <chad@tindel.net>
2023-08-21 10:22:03 -0400

    Added episode 28

--- run_local.sh
+++ run_local.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-docker-buildx build --platform linux/arm64 -t sib:arm .
+#!/bin/bash
+
+# Define your image name and tag
+IMAGE_NAME="sib"
+IMAGE_TAG="arm"
+FULL_IMAGE="$IMAGE_NAME:$IMAGE_TAG"
+
+# Check if the image exists locally
+if ! docker images | grep -E "^$IMAGE_NAME\s+$IMAGE_TAG\s+"; then
+    echo "Image $FULL_IMAGE not found locally. Building using docker buildx..."
+    
+    # Use docker buildx command here
+    docker-buildx build --platform linux/arm64 -t $FULL_IMAGE .
+else
+    echo "Image $FULL_IMAGE is already present."
+fi
+
 docker-compose -f docker-compose.yml up sib
